9938d54c5b1dbf787bb580f1aca6131cb552edc9
rashedul24 <ronyuap@gmail.com>
2021-12-18 22:51:20 +0600

    single category added

--- src/components/SingleCategory/SingleCategory.js
+++ src/components/SingleCategory/SingleCategory.js
@@ -1,22 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const SingleCategory = () => {
-  const { id } = useParams()
-  const [sportDetails, setSportDetails] = useState([])
+  const { id } = useParams();
+  const [sportsDetails, setSportsDetails] = useState([]);
+  const [singleSport, setSingleSport] = useState({});
   useEffect(() => {
     fetch("/sportsDb.json")
-      .then(res => res.json())
-    .then(data=>setSportDetails(data.sports))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setSportsDetails(data.sports));
+  }, []);
   useEffect(() => {
-    const sportInfo = sportDetails.find(sport => sport.idSport === id)
-    console.log(sportInfo)
-  }, [])
-  
+    const game = sportsDetails.find(
+      (sportDetails) => sportDetails?.idSport === id
+    );
+    setSingleSport(game);
+  }, [id, sportsDetails]);
+  console.log(singleSport);
+
   return (
     <div>
-      
+      <div className="container">
+      <div className="card">
+      <img
+            src={singleSport?.strSportThumb}
+            className="card-img-top img-thumbnail card-img"
+            alt="..."
+          />
+          
+          <div className="card-body text-center">
+            <h3 className="card-title">Name: {singleSport?.strSport}</h3>
+            <h4>Icon: <img src={singleSport?.strSportIconGreen} alt="" /></h4>
+            <p>
+              <span className="fw-bold">Format</span>: {singleSport?.strFormat}
+            </p>
+            <p><span className="fw-bold">Description</span>: {singleSport?.strSportDescription}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
